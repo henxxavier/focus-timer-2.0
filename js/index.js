d1990d3678e5108c208c236ef91b7b29554efa30
henxxavier <henxxavier@gmail.com>
2023-01-09 22:09:51 -0300

    add backgrounds

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 //variaveis
 const minutesDisplay = document.querySelector(".minutes")
 const secondsDisplay = document.querySelector(".seconds")
+const twoPoints = document.querySelector(".two-points")
 
 const buttonPlay = document.querySelector(".play")
 const buttonStop = document.querySelector(".stop")
@@ -31,6 +32,18 @@ const resetCards = () => {
 
     buttonFire.classList.remove("color-orange")
     svgFire.classList.remove("svg-white")
+
+    document.querySelector("body").classList.remove("bg-outono", "bg-chuva" ,"bg-cafeteria", "bg-lareira")
+}
+
+const displayColorWhite = () => {
+document.querySelector(".svg-play").classList.add("display-timer-white")
+document.querySelector(".svg-stop").classList.add("display-timer-white")
+document.querySelector(".svg-sum").classList.add("display-timer-white")
+document.querySelector(".svg-sub").classList.add("display-timer-white")
+document.querySelector(".two-points").classList.add("display-timer-white")
+minutesDisplay.classList.add("display-timer-white")
+secondsDisplay.classList.add("display-timer-white")
 }
 
 //eventos
@@ -38,22 +51,34 @@ buttonFall.addEventListener("click", () => {
     resetCards()
     buttonFall.classList.toggle('color-green')
     svgFall.classList.toggle('svg-white')
+
+    document.querySelector("body").classList.add("bg-outono")
+    displayColorWhite()
 })
 
 buttonRain.addEventListener("click", () => {
     resetCards()
     buttonRain.classList.toggle('color-blue')
     svgRain.classList.toggle('svg-white')
+
+    document.querySelector("body").classList.add("bg-chuva")
+    displayColorWhite()
 })
 
 buttonFireplace.addEventListener("click", () => {
     resetCards()
     buttonFireplace.classList.toggle("color-brown")
     svgFireplace.classList.toggle("svg-white")
+
+    document.querySelector("body").classList.add("bg-cafeteria")
+    displayColorWhite()
 } )
 
 buttonFire.addEventListener("click", () => {
     resetCards()
     buttonFire.classList.toggle("color-orange")
     svgFire.classList.toggle("svg-white")
+
+    document.querySelector("body").classList.add("bg-lareira")
+    displayColorWhite()
 })
